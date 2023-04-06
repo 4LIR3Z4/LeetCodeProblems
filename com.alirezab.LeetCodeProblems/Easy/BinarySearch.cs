9aad96916d361232fd085c8a7e86c6a4575b4c98
alireza barkhordari <alirezab.1990@gmail.com>
2023-04-06 17:21:44 +0430

    Add project files.

---
+++ com.alirezab.LeetCodeProblems/Easy/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.alirezab.LeetCodeProblems.Easy;
+internal sealed class BinarySearch
+{
+    public int Search(int[] nums, int target)
+    {
+        int l = 0, h = nums.Length - 1;
+        while (l <= h)
+        {
+            int m = l + (h - l >> 1);
+            if (nums[m] == target)
+            {
+                return m;
+            }
+            if (nums[m] < target)
+            {
+                l = m + 1;
+            }
+            else
+            {
+                h = m - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ com.alirezab.LeetCodeProblems/Program.cs
@@ -0,0 +1,7 @@
+﻿using com.alirezab.LeetCodeProblems.Easy;
+
+int[] arr = { -3, -1, 0, 3, 5, 9, 12, 28, 99, 101 };
+BinarySearch binarySearch = new();
+
+
+Console.WriteLine(binarySearch.Search(arr, 101));
